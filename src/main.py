c909fef266d12d633f78c6d8e7872179325db6b5
izaqyos <47855751+izaqyos@users.noreply.github.com>
2022-12-11 16:56:02 +0200

    Add files via upload

---
+++ src/main.py
@@ -0,0 +1,20 @@
+import sys
+sys.path.append('/Users/i500695/work/code/python/udemy_python_100_days_of_code/day11/blackjack')
+from game import game
+from logger import mylogger
+from src.art import logo
+
+def main():
+    mylogger.debug(f"{main.__qualname__}")
+    print(logo)
+    while True:
+        ans = input("Do you want to play a game of blackjack? Type 'y' or 'n': ")
+        if ans == 'y':
+            agame = game()
+            agame.start()
+        else:
+            return
+
+if __name__ == '__main__':
+    main()
+    
